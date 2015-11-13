b8d39890e2a9bfb90b56b9dbd83bccd32bcdfbd8
fel1200 <fel1200@hotmail.com>
2015-11-13 11:40:26 -0600

    marco

---
+++ app/src/main/java/com/example/admin/inev2/Main2Activity.java
@@ -0,0 +1,76 @@
+package com.example.admin.inev2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+
+public class Main2Activity extends AppCompatActivity {
+    private Toolbar appbar;
+    private Spinner sp1,sp2,sp3,sp4,sp5,sp6,sp7,sp8,sp9,sp10,spb1,spb2,spb3,spb4,spb5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        appbar = (Toolbar)findViewById(R.id.appbar);
+        setSupportActionBar(appbar);
+        //getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_nav_menu);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        sp1=(Spinner)findViewById(R.id.spinnera1);listas(5, sp1);
+        sp2=(Spinner)findViewById(R.id.spinnera2);listas(5, sp2);
+        sp3=(Spinner)findViewById(R.id.spinnera3);listas(5, sp3);
+        sp4=(Spinner)findViewById(R.id.spinnera4);listas(5,sp4);
+        sp5=(Spinner)findViewById(R.id.spinnera5);listas(5, sp5);
+        sp6=(Spinner)findViewById(R.id.spinnera6);listas(5, sp6);
+        sp7=(Spinner)findViewById(R.id.spinnera7);listas(5, sp7);
+        sp8=(Spinner)findViewById(R.id.spinnera8);listas(5, sp8);
+        sp9=(Spinner)findViewById(R.id.spinnera9);listas(5, sp9);
+        sp10=(Spinner)findViewById(R.id.spinnera10);listas(5, sp10);
+
+        spb1=(Spinner)findViewById(R.id.spinnerb1);listas(5,spb1);
+        spb2=(Spinner)findViewById(R.id.spinnerb2);listas(5,spb2);
+        spb3=(Spinner)findViewById(R.id.spinnerb3);listas(5,spb3);
+        spb4=(Spinner)findViewById(R.id.spinnerb4);listas(5,spb4);
+        spb5=(Spinner)findViewById(R.id.spinnerb5);listas(5,spb5);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main2, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void listas(int tam,Spinner sp) {
+        ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < tam; i++) {
+            list.add("items" + i);
+        }
+
+        ArrayAdapter adp = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
+        sp.setAdapter(adp);
+    }
+}
---
+++ app/src/main/java/com/example/admin/inev2/MainActivity.java
@@ -0,0 +1,190 @@
+package com.example.admin.inev2;
+
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+    UserSessionManager session;
+
+    private Toolbar appbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navView;
+    ExpandableListView elv;
+    ListView lvmenu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        appbar = (Toolbar)findViewById(R.id.appbar);
+        setSupportActionBar(appbar);
+
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_nav_menu);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+
+        navView = (NavigationView)findViewById(R.id.navview);
+        navView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+                        boolean fragmentTransaction = false;
+                        Fragment fragment = null;
+
+                        switch (menuItem.getItemId()) {
+                            case R.id.itmem1:
+                                fragment = new Fragment1();
+                                fragmentTransaction = true;
+                                break;
+                            case R.id.item2:
+                                fragment = new Fragment2();
+                                fragmentTransaction = true;
+                                break;
+                            case R.id.item3:
+                                fragment = new Fragment3();
+                                fragmentTransaction = true;
+                                break;
+                            case R.id.item4:
+                                Log.i("NavigationView", "Pulsada opción 4");
+                                menuItem.setChecked(true);
+                                getSupportActionBar().setTitle(menuItem.getTitle());
+
+                                break;
+                            case R.id.item5:
+                                Log.i("NavigationView", "Pulsada opción 5");
+                                menuItem.setChecked(true);
+                                getSupportActionBar().setTitle(menuItem.getTitle());
+
+                                break;
+                            case R.id.item6:
+                                Log.i("NavigationView", "Pulsada opción 6");
+                                menuItem.setChecked(true);
+                                getSupportActionBar().setTitle(menuItem.getTitle());
+
+                                break;
+                            case R.id.nav_log_out:
+                                session.logoutUser();
+                                Log.i("NavigationView", "Cerrar session");
+                                break;
+                        }
+
+                        if(fragmentTransaction) {
+                            getSupportFragmentManager().beginTransaction()
+                                    .replace(R.id.content_frame, fragment)
+                                    .commit();
+
+                            menuItem.setChecked(true);
+                            getSupportActionBar().setTitle(menuItem.getTitle());
+                        }
+
+                        drawerLayout.closeDrawers();
+
+                        return true;
+                    }
+                });
+
+        ///////////////////////////////////////////////////////////////////////////////////////////
+        //Intent intent = new Intent(this, LoginActivity.class);
+        //startActivity(intent);
+        // Session class instance
+        session = new UserSessionManager(getApplicationContext());
+
+        TextView lblName = (TextView) findViewById(R.id.username);
+        TextView lblEmail = (TextView) findViewById(R.id.email);
+
+
+
+        Toast.makeText(getApplicationContext(),
+                "User Login Status: " + session.isUserLoggedIn(),
+                Toast.LENGTH_SHORT).show();
+
+
+
+        // Check user login (this is the important point)
+        // If User is not logged in , This will redirect user to LoginActivity
+        // and finish current activity from activity stack.
+        if(session.checkLogin())
+            finish();
+
+        // get user data from session
+        HashMap<String, String> user = session.getUserDetails();
+
+        // get name
+        String name = user.get(UserSessionManager.KEY_NAME);
+
+        // get email
+        String email = user.get(UserSessionManager.KEY_EMAIL);
+
+        // Show user data on activity
+        // lblName.setText(Html.fromHtml("Name: <b>" + name + "</b>"));
+        //lblEmail.setText(Html.fromHtml("Email: <b>" + email + "</b>"));
+
+        lblEmail.setText("");
+        lblName.setText("");
+        lblName.setText( name);
+        lblEmail.setText(email);
+
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+       // int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        //if (id == R.id.action_settings) {
+          //  return true;
+        //}
+        switch (item.getItemId()){
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    public void prueba(View v){
+        Intent act=new Intent(this,Main2Activity.class);
+        Toast.makeText(this,"flotante",Toast.LENGTH_SHORT).show();
+        startActivity(act);
+       // Snackbar.make(v, "Replace with your own action", Snackbar.LENGTH_LONG)
+               // .setAction("Action", null).show();
+    }
+
+}
