909b1e93769cf4ea18f9696ce6dd3a9bb1e86912
marcomark <markz0202a@gmail.com>
2015-12-03 10:15:38 -0600

    prueba1 031215

--- app/src/main/java/com/example/admin/inev2/FragmenInforme.java
+++ app/src/main/java/com/example/admin/inev2/FragmenInforme.java
@@ -16,6 +16,6 @@ public class FragmenInforme extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.framinforme, container, false);
+        return inflater.inflate(R.layout.fragment_prueba, container, false);
     }
 }
