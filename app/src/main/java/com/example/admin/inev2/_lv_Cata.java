3ff245bff67a9b325aa14f49d1545e950cdd8c2a
marcomark <marco.melo@ine.mx>
2017-04-24 11:22:08 -0500

    21-04-17

---
+++ app/src/main/java/com/example/admin/inev2/_lv_Cata.java
@@ -0,0 +1,41 @@
+package com.example.admin.inev2;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import static com.ine.admin.observav1.MainActivity.g_contexto;
+
+/**
+ * Created by INE on 01/03/2017.
+ */
+
+public class _lv_Cata {
+    ArrayList<String> a_Cat;
+    ArrayAdapter<String> ad_Cat;
+
+    //private BD_Pto db;
+    private Context contexto;
+    public ListView _lv;
+
+    public _lv_Cata(ArrayList<String> a_Cat) {
+        this.contexto   = g_contexto;
+        this.a_Cat      = a_Cat;
+        this._cursor();
+    }
+
+    public void _cursor(){
+
+        ad_Cat = new ArrayAdapter<String>(contexto,android.R.layout.select_dialog_multichoice,a_Cat);
+        _lv    = new ListView(this.contexto);
+
+        _lv.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        _lv.setBackgroundColor(Color.LTGRAY);
+        _lv.setAdapter(ad_Cat);
+        _lv.setTextFilterEnabled(true);
+    }
+
+}
